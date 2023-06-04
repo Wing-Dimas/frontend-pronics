8d68f64b3a761201642f7b1d85dc93c5ae69c0b5
Wing-Dimas <wingdimas@gmail.com>
2023-06-04 11:14:36 +0700

    first commit

---
+++ components/Home/Header.jsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+export default function Home() {
+  return (
+    <div>
+      <Image
+        src="/assets/img/logo_pronics.png"
+        width={274}
+        height={89}
+        alt="logo_pronics"
+      />
+    </div>
+  );
+}
